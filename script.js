117ac916f6455c34ff24a1060d5b0f510c77835b
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2023-09-26 18:32:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.warn("Lancer got ur ip addresses1!!111")
-console.warn("also this website based on http://ip-api.com. So limit request of this is 50 request/minutes")
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "http://ip-api.com/json");
+xhr.send();
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const contents = `* ${JSON.parse(xhr.response).query}`
+    ip.innerText = contents
+
+  } else {
+    alert(`[${xhr.status}] Oh no, you so good that even lancer can't get your ip addresses :(`);
+  }
+};
